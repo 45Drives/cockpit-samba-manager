7f793b0e439b3d02bbc050f08d70906031a75038
joshuaboud <joshuaboud@hotmail.com>
2021-04-08 15:40:48 -0300

    remove centos name from EL packaging, set up EL8 packaing

--- package-el7.sh
+++ package-el7.sh
@@ -30,7 +30,7 @@ command -v docker > /dev/null 2>&1 || {
 
 # if docker image DNE, build it
 if [[ "$(docker images -q cockpit-samba-manager-el7-builder 2> /dev/null)" == "" ]]; then
-	docker build -t cockpit-samba-manager-el7-builder - < docker/centos7
+	docker build -t cockpit-samba-manager-el7-builder - < docker/el7
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo "Building docker image failed."
@@ -40,7 +40,7 @@ fi
 
 mkdir -p dist/{el7,tmp}
 
-SOURCE_DIR_NAME=cockpit-samba-manager-$(grep Version centos7/cockpit-samba-manager.spec --color=never | awk '{print $2}')
+SOURCE_DIR_NAME=cockpit-samba-manager-$(grep Version el/cockpit-samba-manager.spec --color=never | awk '{print $2}')
 SOURCE_DIR=dist/tmp/$SOURCE_DIR_NAME
 mkdir -p $SOURCE_DIR
 
@@ -51,7 +51,7 @@ tar -czvf $SOURCE_DIR_NAME.tar.gz $SOURCE_DIR_NAME
 popd
 
 # build rpm from source tar and place it dist/el7 by mirroring dist/el7 to rpmbuild/RPMS
-docker run -u $(id -u):$(id -g) -w /home/rpm/rpmbuild -it -v$(pwd)/dist/tmp:/home/rpm/rpmbuild/SOURCES -v$(pwd)/dist/el7:/home/rpm/rpmbuild/RPMS -v$(pwd)/centos7:/home/rpm/rpmbuild/SPECS --rm cockpit-samba-manager-el7-builder rpmbuild -ba SPECS/cockpit-samba-manager.spec
+docker run -u $(id -u):$(id -g) -w /home/rpm/rpmbuild -it -v$(pwd)/dist/tmp:/home/rpm/rpmbuild/SOURCES -v$(pwd)/dist/el7:/home/rpm/rpmbuild/RPMS -v$(pwd)/el:/home/rpm/rpmbuild/SPECS --rm cockpit-samba-manager-el7-builder rpmbuild -ba SPECS/cockpit-samba-manager.spec
 res=$?
 if [ $res -ne 0 ]; then
 	echo "Packaging failed."
